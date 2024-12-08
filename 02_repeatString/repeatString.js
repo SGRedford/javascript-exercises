1a23528fec817db78fc4244c20a46407fcf2d70f
SGRedford <sean.redford@gmail.com>
2024-12-08 15:21:13 -0700

    Added code to complete tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, times) {
+  let result = "";
+  if (times < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < times; i++) {
+    result += string;
+  }
+  return result;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+git add repeatString.js repeatString.spec.js
